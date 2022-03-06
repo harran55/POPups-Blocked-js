b09cf790e1bdfbc732a3de868a4b15304a01109d
harran55 <h.almarra@gmail.com>
2022-03-06 14:32:00 +0000

    Update popupsjs.user.js

--- popupsjs.user.js
+++ popupsjs.user.js
@@ -5,20 +5,7 @@
 // @version      v1
 // @description  Page my https://io.hsoub.com/u/h1om
 // @author       harran El-marri
-// @include      https://animelek.me/*
-// @include      https://witanime.com/*
-// @include      https://www.arabanime.net/*
-// @include      https://*.animelek.tv/*
-// @include      https://*.cloud-anime.com/*
-// @include      https://animespire.buzz/*
-// @include      https://www.animeiat.tv/*
-// @include      https://yo-anime.com/*
-// @include      https://www.ibranime.com/*
-// @include      https://www.egyanime.com/*
-// @include      https://www.anmespire.com/*
-// @include      https://animerco.com/*
-// @include      https://a.xsanime.com/*
-// @include      https://an-master.net/*
+// @match		*://*/*
 // @icon         https://cdn-icons-png.flaticon.com/128/3162/3162620.png
 // @grant        none
 // @require      http://code.jquery.com/jquery-3.4.1.min.js
@@ -30,21 +17,12 @@
         $('html body').append('<a href="https://io.hsoub.com/u/h1om" style="z-index: 9999;position: fixed;top: 0;right: 0;'
                               + 'padding: 10px;background: rgba(0, 0, 0, .8);color: white;font-family: monospace;">'
                               + ' 🥳 harran el-marri</a>');
-        $('div[style=z-index: 2147483647]').remove();
-        $('center#h860').remove();
         $('script').remove();
-        $('iframe[allowtransparency=true]').remove();
-        $('iframe[src=about:blank]').remove();
         $('html > iframe').remove();
         $('body > iframe').remove();
+        $('iframe[allowtransparency=true]').remove();
+        $('iframe[src=about:blank]').remove();
+        $('div[style=z-index: 2147483647]').remove();
+        $('center#h860').remove();
     });
-    setInterval(() => {
-        $('div[style=z-index: 2147483647]').remove();
-        $('center#h860').remove();
-        $('script').remove();
-        $('iframe[allowtransparency=true]').remove();
-        $('iframe[src=about:blank]').remove();
-        $('html > iframe').remove();
-        $('body > iframe').remove();
-    }, 100);
 })();
